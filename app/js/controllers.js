e13866c9f4e2ff86ef13f14561697006ab5e73a5
Bojan Mihelac <bmihelac@mihelac.org>
2013-07-07 09:08:26 +0200

    Add persistence service

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,11 +3,37 @@
 /* Controllers */
 
 angular.module('myApp.controllers', []).
-  controller('MyCtrl1', ['$scope', 'version', 'today', function($scope, version, today) {
-    $scope.greet = today;
-    console.log($scope);
+controller('MyCtrl1',
+  ['$scope', 'tomorrow', 'version', 'today', 'greeter', 'persistencejs', function($scope, tomorrow, version, today, greeter, persistencejs) {
+    $scope.today = today;
+    $scope.tomorrow = tomorrow;
+    $scope.greet = greeter.greet('bojan');
 
+    $scope.delete = function(id) {
+      persistencejs.models.Todo.all()
+      .filter('id', '=', id).destroyAll(null, function() {
+        $scope.getObjectList();
+      });
+    };
+
+    $scope.insert = function(object) {
+      var todo = new persistencejs.models.Todo(object);
+      persistencejs.persistence.add(todo);
+      persistencejs.persistence.flush(function() {
+        $scope.getObjectList();
+      });
+    };
+
+    $scope.getObjectList = function() {
+      persistencejs.models.Todo.all().list(null, function(results) {
+        $scope.objectList = results;
+        $scope.$apply();
+      });
+    };
+
+    $scope.getObjectList();
   }])
+
   .controller('MyCtrl2', [function() {
 
-  }]);
+}]);
--- app/js/services.js
+++ app/js/services.js
@@ -2,17 +2,36 @@
 
 /* Services */
 
-function Greeter(a) {
-  this.greet = function() {
+function Greeter() {
+  this.greet = function(a) {
     return 'Hello ' + a;
-  }
+  };
 }
 
 // Demonstrate how to register services
 // In this case it is a simple value service.
 angular.module('myApp.services', []).
   value('version', '0.1').
+  factory('tomorrow', function() {
+    return new Date();
+  }).
   factory('today', function() {
     return new Date();
   }).
-  service('greeter', Greeter);
+  factory('greeter', function() {
+    return new Greeter();
+  }).
+  factory('persistencejs', function() {
+    persistence.store.websql.config(persistence, 'todos-angularjs', 'todo database', 5*1024*1024);
+    var models = {};
+    models.Todo = persistence.define('todo', {
+      title: 'TEXT',
+      completed: 'BOOL'
+    });
+    persistence.schemaSync();
+
+    return {
+      persistence: persistence,
+      models: models
+    };
+  });
